1baff5b0e61ac8018363ae2beacc1ca4c1177577
David R. Poindexter III <drpoindexter@gmail.com>
2017-02-27 18:03:33 -0500

    Add model, initial data, and render initial data

--- Todo.elm
+++ Todo.elm
@@ -5,13 +5,34 @@ main =
     view
 
 --model
+type alias Model =
+    { todo: String
+    }
+
+model =
+    { todo = ""
+    , todos = [ "Buy Milk", "Order medicine", "Walk the dog" ]
+    }
 
 --update
 
+
+
 --view
+
+todoItem todo =
+    li []
+        [ text todo ]
+
+todoList todos =
+    let
+        children = List.map todoItem todos
+    in
+        ul [] children
 
 view =
     div []
         [ input [ type_ "text" ] []
         , button [] [ text "Add Todo" ]
+        , todoList model.todos
         ]
